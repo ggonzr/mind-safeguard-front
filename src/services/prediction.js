87e678c5bdd08f074fcc487549e1d91f7c1c627f
Geovanny González-Rodríguez <ga.gonzalezr99@gmail.com>
2022-04-20 01:24:48 -0500

    Logo & GCP endpoint

--- src/services/prediction.js
+++ src/services/prediction.js
@@ -5,7 +5,7 @@ import axios from "axios";
 import request from "../data/request.json";
 
 // URL endpoint
-const URL = "http://127.0.0.1:8000/";
+const URL = "https://prediction-ywhsiiswia-uc.a.run.app/";
 
 // Make the prediction
 export const makePrediction = async (body) => {
